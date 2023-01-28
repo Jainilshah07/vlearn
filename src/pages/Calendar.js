d06a7938fca0858884be5ed7f72d468d83f8921c
Saloni <salonishah306@gmail.com>
2023-01-28 23:47:35 +0530

    Called Sidebar in pages

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+
 
 const Calendar = () => {
   return (
-    <div>
-      
+    <div className="grid grid-col-10">
+      <div className="col-span-2">
+        <Sidebar />
+      </div>
+      <div className="col-span-8">
+        
+      </div>
     </div>
   )
 }
 
-export default Calendar
+export default Calendar;
