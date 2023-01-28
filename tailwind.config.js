7710595e069eddfe68ec9ce6e097bfa190aff887
AAKASH GORADIA <80081102+AakashGoradia@users.noreply.github.com>
2023-01-28 17:09:20 +0530

    signup page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:
+         {
+           'hero': "url('https://img.freepik.com/free-vector/gradient-abstract-colorful-shapes-background_23-2149120340.jpg?w=900&t=st=1674781379~exp=1674781979~hmac=5ebd99d809c178d70fbd1118a81be626c10e447cb6447fc3c325a9d3f9593adc')"
+         }
+    },
   },
   plugins: [],
 }
